9261058ce11f725ccde1e5feae10945a0d78ff85
Udhayakumar <udhayakumar.p@accionlabs.com>
2018-09-05 13:00:46 +0530

    Added footer

--- src/shell/containers/Footer.js
+++ src/shell/containers/Footer.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import '../style.scss'
 import { Row, Col } from 'antd';
-import MarketPlaceHomeFooter from '../../modules/marketplace/home/components/HomeFooter'
 
 class Footer extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             currentPage: 'default'
         }
     }
-    render(){
-        return(
-            <Row>
-                <Col span={24}>
-                    <MarketPlaceHomeFooter />
-                </Col>
-            </Row>
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col span={24} >
+                        <div style={{ height: '300px', backgroundColor: '#202020' }} />
+                        <div style={{height:'96px', display:'flex', color:'#ffffff', flexDirection:'column',alignItems:'center', justifyContent:'center',backgroundColor:'#181818'}}>
+                         <label>Live Editions, Inc. © 2018 All right reserved | 2890 Kilgore Rd, Suite 165 Rancho Cordova CA 95670 |</label>
+                         <label>www.liveditions.com</label>
+                        </div>
+                    </Col>
+
+                </Row>
+            </div>
         )
     }
 }
-
 export default Footer
